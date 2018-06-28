ccb090ae5fea0920a4bf727d6d394483109e2548
indranil goswami <indranil.goswami14@gmail.com>
2018-06-28 18:25:12 +0530

    new commit

---
+++ src/main/java/com/learning/git/SpringRest2/SpringRest2Application.java
@@ -0,0 +1,12 @@
+package com.learning.git.SpringRest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRest2Application.class, args);
+	}
+}
